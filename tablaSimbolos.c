4dadf89c2f2b757bd18f58f0c8d6324a8edf35a5
Fernando Ares <ferares4@gmail.com>
2015-10-22 12:05:13 -0300

    Traslado funciones a tabla de simbolos y corrigo el codigo en yacc

--- tablaSimbolos.c
+++ tablaSimbolos.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
 typedef enum symbol_type {
   T_NUMBER,
   T_BOOLEAN,
@@ -5,11 +8,100 @@ typedef enum symbol_type {
 } SymbolType;
 
 typedef struct variable {
-  char name[50];
-  SymbolType type;
+  char nombre[255];
+  SymbolType tipo;
 } Variable;
 
 typedef struct tabla {
-  char name[50];
-  variable variables[50];
+  Variable variables[50];
 } Tabla;
+
+/*https://en.wikipedia.org/wiki/C_dynamic_memory_allocation*/
+
+/*Variables Globales*/
+Tabla *tablaDeSimbolos;
+
+/*Definicion de funciones*/
+Tabla crear();
+void insertar(char nombre[255],char tipo);
+void remover(char nombre[255]);
+char getTipo(char nombre[255]);
+int existe(char nombre[255]);
+
+
+/*Implementacion de funciones*/
+
+/* Crea una tabla de signos y reserva espacio en memoria*/
+Tabla crear(){
+     
+     tablaDeSimbolos = malloc(sizeof (Tabla));
+     return *tablaDeSimbolos;
+     
+}
+     
+void insertar(char nombre[255],char tipo){
+
+     if (existe(nombre)==0){
+      
+         int i = 0;
+         Variable *variable = malloc(sizeof (Variable));
+      // strcpy(*variable.nombre,nombre);
+      //   variable.tipo = tipo;
+         
+         for (i; i<50; i++){
+         
+         /*
+             if (tablaDeSimbolos.variables[i].nombre==NULL){
+                                                     
+             tablaDeSimbolos.variables[i] = *variable;
+             
+             }
+         */    
+         }                                           
+                                                 
+     }
+
+}
+
+void remover(char nombre[255]){
+     
+     if (existe(nombre)==1){
+      
+      /*Remover*/                                            
+                                                 
+     }
+     
+}
+
+char getTipo(char nombre[255]){
+     
+     if (existe(nombre)==1){
+      
+      //Recorrer vector y devolver tipo                                    
+                                                 
+     }
+     
+}
+
+int existe(char nombre[255]){
+        
+       int i=0; 
+        
+       for (i; i<50; i++){
+           
+         /*  if (strncmp(tablaDeSimbolos.variables[i].nombre, nombre, 255)==0){
+                                                 
+           return 1;
+           
+           }
+          */ 
+       }
+       
+       return 0;
+        
+}
+
+main(){
+       
+       
+}
