efb27e254c68037a26ff7cf165e6ac8bf5cb2e9d
Fernando Ares <ferares4@gmail.com>
2015-10-23 10:48:31 -0300

    Implementacion de Tabla

--- tablaSimbolos.c
+++ tablaSimbolos.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-typedef enum symbol_type {
-  T_NUMBER,
-  T_BOOLEAN,
-  T_STRING
-} SymbolType;
-
 typedef struct variable {
   char nombre[255];
-  SymbolType tipo;
+  char tipo;
 } Variable;
 
 typedef struct tabla {
-  Variable variables[50];
+  Variable * variables[50];
 } Tabla;
 
 /*https://en.wikipedia.org/wiki/C_dynamic_memory_allocation*/
 
-/*Variables Globales*/
-Tabla *tablaDeSimbolos;
+Tabla tablaDeSimbolos;
 
 /*Definicion de funciones*/
 Tabla crear();
-void insertar(char nombre[255],char tipo);
+int insertar(char nombre[255],char tipo);
 void remover(char nombre[255]);
 char getTipo(char nombre[255]);
 int existe(char nombre[255]);
@@ -33,33 +26,43 @@ int existe(char nombre[255]);
 
 /* Crea una tabla de signos y reserva espacio en memoria*/
 Tabla crear(){
-     
-     tablaDeSimbolos = malloc(sizeof (Tabla));
-     return *tablaDeSimbolos;
+
+      int i=0;
+      for (i; i<50; i++){
+        
+        tablaDeSimbolos.variables[i] = NULL;  
+      
+      }
+
+      return tablaDeSimbolos;
      
 }
      
-void insertar(char nombre[255],char tipo){
+int insertar(char nombre[255],char tipo){
 
      if (existe(nombre)==0){
-      
-         int i = 0;
-         Variable *variable = malloc(sizeof (Variable));
-      // strcpy(*variable.nombre,nombre);
-      //   variable.tipo = tipo;
+     
+                            
+             Variable * temp;
+             temp = malloc(sizeof(Variable));
+             strcpy(temp->nombre,nombre);
+             temp->tipo = tipo;
+
+             int i = 0;
+             for (i; i<50; i++){
          
-         for (i; i<50; i++){
-         
-         /*
-             if (tablaDeSimbolos.variables[i].nombre==NULL){
+             if (tablaDeSimbolos.variables[i] == NULL){       
                                                      
-             tablaDeSimbolos.variables[i] = *variable;
-             
+                 tablaDeSimbolos.variables[i] = temp;
+                 return 1;
+
              }
-         */    
+
          }                                           
                                                  
      }
+     
+     return 0;
 
 }
 
@@ -73,35 +76,79 @@ void remover(char nombre[255]){
      
 }
 
+
 char getTipo(char nombre[255]){
      
      if (existe(nombre)==1){
       
-      //Recorrer vector y devolver tipo                                    
-                                                 
+        int i=0; 
+        
+        for (i; i<50; i++){
+            
+           if (tablaDeSimbolos.variables[i]!=NULL && strcmp(tablaDeSimbolos.variables[i]->nombre, nombre)==0){
+
+             return tablaDeSimbolos.variables[i]->tipo;
+                                                      
+            }
+           
+            
+        }                                                 
      }
      
+     return 'u';   
 }
 
 int existe(char nombre[255]){
-        
-       int i=0; 
-        
+
+       int i=0;
+      
        for (i; i<50; i++){
-           
-         /*  if (strncmp(tablaDeSimbolos.variables[i].nombre, nombre, 255)==0){
-                                                 
-           return 1;
-           
-           }
-          */ 
+         
+         if (tablaDeSimbolos.variables[i]!=NULL){
+             
+             if (strcmp(tablaDeSimbolos.variables[i]->nombre, nombre)==0){
+                                  
+               return 1;
+               
+               }
+
+         }
        }
        
        return 0;
         
 }
 
+void test(){
+
+     printf("Creando tabla... \n");
+     crear();
+     printf("Tabla creada con exito! \n");     
+     printf("Insertando variables en tabla... \n");
+     insertar("a",'n');
+     insertar("b",'n');
+     insertar("pruebaString",'s');
+     insertar("a",'n');
+     insertar("b",'n');
+     insertar("b",'n');
+     insertar("pruebaBoolean",'b');
+     printf("Variables insertadas con exito! \n");    
+     printf("Validando valores insertados... \n");
+     int i=0;
+     for (i;i<10;i++){
+      
+         if (tablaDeSimbolos.variables[i]!=NULL){
+            printf("Tabla de simbolos en posicion %d | %s | %c \n",i,tablaDeSimbolos.variables[i]->nombre, getTipo(tablaDeSimbolos.variables[i]->nombre));
+            }
+     }
+     printf("Test concluido! \n");
+     
+     
+}
+
 main(){
-       
+
+    test();
+    system("PAUSE");
        
 }
